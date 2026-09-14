cf8fa98b38d9a0f4f2e4a776867e3086d9a76396
limengxun <limengxun45@outlook.com>
2026-09-14 23:44:52 +0800

    limit z search size

--- src/xun/tools/_extra.py
+++ src/xun/tools/_extra.py
@@ -15,37 +15,46 @@ def _expose_z_search():
         return []
 
     @tool_attr(name="web_search", override=True)
-    def z_search(query: str, limit: int = 5, content_size: Literal["short", "medium"] = "short") -> list[dict]:
+    def z_search(
+        query: str, 
+        limit: int = 5, 
+        max_content_length: int = 4096,
+        ) -> list[dict]:
         """
         Web search with ZhipuAI's Web Search tool. 
         Prefer this tools for web search tasks, get more accurate and comprehensive search results.
 
-        Only "short" and "medium" content sizes are supported to keep the context manageable.
+        The maximum content length (per result, in characters) can be controlled via the `max_content_length` parameter.
         For details on any result, visit the link provided in the "link" field.
         """
+        # THE API IS REALLY BAD AT DOCUMENTING... ALSO UNPREDICTABLE BEHAVIOR
         # https://docs.bigmodel.cn/cn/guide/tools/web-search
-        # content size can be long... but that will be too much for context
 
         response = z_client.web_search.web_search(
             search_engine="search_pro",
             search_query=query,
             count=limit,
             search_recency_filter="noLimit",
-            content_size=content_size
+
+            # content size can be long... but that will be too much for context
+            content_size='medium'
         )
 
-        MAX_LEN = 10240  # 限制content字段的长度，避免过长的内容导致后续处理困难
+        MAX_LEN = max_content_length  # 限制content字段的长度，避免过长的内容导致后续处理困难
         results = []
         search_result = response.search_result
         if search_result and isinstance(search_result, list):
-            for item in search_result:
+            for i, item in enumerate(search_result):
                 results.append({
                     "title": item.title,                # type: ignore
                     "link": item.link,                  # type: ignore
-                    "content": item.content[:MAX_LEN] + ("... (内容过长被截断)" if len(item.content) > MAX_LEN else ""),  # type: ignore
+                    "content": item.content[:MAX_LEN] + ("... [truncated]" if len(item.content) > MAX_LEN else ""),  # type: ignore
                     "media": item.media,                # type: ignore
                     "publish_date": item.publish_date   # type: ignore
                 })
+                if i > limit - 1:
+                    # zai will somehow over-return more results than requested...
+                    break
         else:
             raise AssertionError("Unexpected response format from ZhipuAI Web Search tool: 'search_result' is missing or not a list.")
         return results
