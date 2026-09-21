a62e0b5055a7635cb3e5ff14d6da50457a488df2
limengxun <limengxun45@outlook.com>
2026-09-21 15:19:33 +0800

    minor

--- extensions/web_display_log.py
+++ extensions/web_display_log.py
@@ -11,12 +11,8 @@ def setup_extension(ctx: ExtensionContext) -> None:
     def _print(msg: str):
         print(msg)
     
-    agent_id = f"{ctx.agent.name} ({ctx.agent.identifier})"
+    agent_id = f"{ctx.agent.name}"
     
-    ctx.agent.hooks.before_execution.add(lambda args: _print(
-        f"{agent_id} Executing: {args.agent.conversation.messages[-1] if args.agent.conversation.messages else 'No messages'}"
-    ))
-
     ctx.agent.hooks.before_display_error.add(lambda args: _print(
-        f"{agent_id} Displaying Error: {args.message}"
+        f"{agent_id} Error: {args.message}"
     ))
